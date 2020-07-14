e56aa8fce973e60313e941fb5648b1ebbdc81d91
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-07-14 18:04:44 -0300

    ✨ Add constraints to bundle serve

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import Middewares from "Middlewares"
 Middewares(app)
 
 // server configuration
-const PORT = process.env.PORT
+const { PORT = "8000", NODE_ENV = "development" } = process.env;
 
 // import routes of API
 import Router from 'Routes'
@@ -22,21 +22,26 @@ db.sync().then(() => {
 
   // create a route for the app
   app.get('/api', (req, res) => {
-    res.json({
-      message: "this is my starter project for a Node.js API with a postgres server connection",
-      PS: "please remember to set up env vars in ./.env (example is in ./env.example",
-    })
+    if (NODE_ENV !== "production") {
+      res.json({
+        message: "this is my starter project for a Node.js API with a postgres server connection",
+        PS: "please remember to set up env vars in ./.env (example is in ./env.example",
+      })
+    }
   })
 
   // any requests that dont have an API path, map to react bundle
-  app.use(express.static(path.join(__dirname, '/app/build')));
-  app.get('*', (req, res) => {
-    console.log("going to the app, not the API", path.join(__dirname, '/app/build', 'index.html'))
-    res.sendFile(path.join(__dirname, '/app/build', 'index.html'))
-  })
+  if (NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '/app/build')));
+    app.get('*', (req, res) => {
+      console.log("going to the app, not the API", path.join(__dirname, '/app/build', 'index.html'))
+      res.sendFile(path.join(__dirname, '/app/build', 'index.html'))
+    })
+  }
   
   // make the server listen to requests
   app.listen(PORT, () => {
     console.log(`Server running at: http://localhost:${PORT}/`)
   })
+
 })
