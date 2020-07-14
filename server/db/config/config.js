28251e420536561063ba6c30fb138312f61e2e72
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-07-14 01:08:18 -0300

    🔧 Update .sequelizerc

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -1,25 +1,14 @@
-require('dotenv').config()
+const dotenv = require("dotenv");
+dotenv.config();
 
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PWD,
-    "database": process.env.DB_NAME,
-    "host":     process.env.DB_HOST,
-    "dialect": "postgres",
+  dialect: "postgres",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  define: {
+    timestamps: true,
+    underscored: true,
   },
-  "test": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PWD,
-    "database": process.env.DB_NAME,
-    "host":     process.env.DB_HOST,
-    "dialect": "postgres",
-  },
-  "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PWD,
-    "database": process.env.DB_NAME,
-    "host":     process.env.DB_HOST,
-    "dialect": "postgres",
-  }
-}
+};
