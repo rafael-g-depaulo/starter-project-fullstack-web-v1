bd97b8d4eb68315fbfa43d3a896399a8d8226505
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-07-14 17:31:59 -0300

    ✨ Add middlewares setup file

--- server/index.js
+++ server/index.js
@@ -5,24 +5,9 @@ import path from 'path'
 // create new express app and save it as "app"
 const app = express()
 
-// get express middlewares
-import cors from 'cors'
-
-const allowedOrigins = [
-  'http://localhost:3000',
-]
-app.use(cors({
-  origin: (origin, callback) => {
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.'
-      return callback(new Error(msg), false)
-    }
-    return callback(null, true)
-  }
-}))
+// setup middlewares
+import Middewares from "Middlewares"
+Middewares(app)
 
 // server configuration
 const PORT = process.env.PORT
