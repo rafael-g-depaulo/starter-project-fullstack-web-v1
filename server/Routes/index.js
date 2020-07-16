c255b5a887bd5ea76d0e2a9f31f07e71bfa972af
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-07-15 22:03:35 -0300

    🔥 Remove all models and migrations

--- server/Routes/index.js
+++ server/Routes/index.js
@@ -1,10 +1,10 @@
 import express from 'express'
 
-import EmailRouter from './email'
+// import EmailRouter from './email'
 
-import Email from "Models/Email"
+// import Email from "Models/Email"
 
 // use dependency injection in module
 export default ({ }, config = { mergeParams: true }) => express.Router(config)
 // add routes
-  .use('/email', EmailRouter({ Email }, config))
+  // .use('/email', EmailRouter({ Email }, config))
