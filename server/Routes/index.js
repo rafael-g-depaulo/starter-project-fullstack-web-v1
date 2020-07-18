49d910d6fc34b29f3a142a42222f9e2a367e7183
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-07-18 17:37:06 -0300

    ✨ 🐛 Make basic Model stuff work

--- server/Routes/index.js
+++ server/Routes/index.js
@@ -2,7 +2,8 @@ import express from 'express'
 
 import DragonRouter from './dragon'
 
-import Dragon from "Models/dragon"
+// import Dragon from "Models/dragon"
+import { Dragon } from "Models"
 
 // use dependency injection in module
 export default ({ }, config = { mergeParams: true }) => express.Router(config)
