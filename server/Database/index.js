d11bb0b88119d0be5aeec571018346b175ad320c
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-07-18 14:39:20 -0300

    🔥 Remove all models

--- server/Database/index.js
+++ server/Database/index.js
@@ -1,11 +1,11 @@
 import Sequelize from "sequelize"
 import dbConfig from "./config"
 
-import DragonModel from "Models/dragon"
+// import DragonModel from "Models/dragon"
 
 const connection = new Sequelize(dbConfig)
 
-DragonModel.init(connection)
-DragonModel.associate(connection.models)
+// DragonModel.init(connection)
+// DragonModel.associate(connection.models)
 
 export default connection
