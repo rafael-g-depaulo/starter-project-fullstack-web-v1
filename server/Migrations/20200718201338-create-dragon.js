8f71538db29d1ecffc3db3fef0b24af3dafca473
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-07-18 17:24:19 -0300

    ✨ Add options to dragon model

--- server/Migrations/20200718201338-create-dragon.js
+++ server/Migrations/20200718201338-create-dragon.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Dragons', {
+    await queryInterface.createTable('dragons', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -11,17 +11,17 @@ module.exports = {
       name: {
         type: Sequelize.STRING
       },
-      createdAt: {
+      created_at: {
         allowNull: false,
         type: Sequelize.DATE
       },
-      updatedAt: {
+      updated_at: {
         allowNull: false,
         type: Sequelize.DATE
       }
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Dragons');
+    await queryInterface.dropTable('dragons');
   }
 };
--- server/Models/dragon.js
+++ server/Models/dragon.js
@@ -16,9 +16,33 @@ module.exports = (sequelize, DataTypes) => {
   };
   Dragon.init({
     name: DataTypes.STRING
-  }, {
+  },
+  // options
+  {
+    // need to pass the connection instance
     sequelize,
-    modelName: 'Dragon',
+
+    // model name
+    modelName: "Dragon",
+
+    // define the table name
+    tableName: 'dragons',
+
+    // if true, add timestamps to table (updatedAt, createdAt)
+    timestamps: true,
+
+    // if true, doesn't delete rows, instead adds a deletedAt column, if timestamps is true
+    paranoid: false,
+
+    // if true, uses snake_case instead of camelCase to generated attributes
+    // ex: created_at instead of createdAt
+    underscored: true,
+
+    // hooks
+    hooks: {
+      // beforeCreate: (user, options) => user.id = uuid()
+
+    }
   });
   return Dragon;
 };
