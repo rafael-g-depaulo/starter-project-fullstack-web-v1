0ded23011e1eeac4ffa1ff1120bf64458df3f824
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-07-23 14:56:57 -0300

    ✨ Add basic user model migration

--- server/Migrations/20200723171811-create-user.js
+++ server/Migrations/20200723171811-create-user.js
@@ -1,33 +1,38 @@
 'use strict';
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       name: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
+        type: Sequelize.STRING,
         allowNull: false,
-        type: Sequelize.DATE
       },
-      updatedAt: {
+      created_at: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
       }
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('users');
   }
 };
--- server/Models/user.js
+++ server/Models/user.js
@@ -1,25 +1,44 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
-};
+import { Model } from "sequelize"
+
+export class User extends Model {
+  static init(sequelize, DataTypes) {
+    super.init({
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      name: DataTypes.STRING,
+    },
+    // options
+    {
+      // need to pass the connection instance
+      sequelize,
+  
+      // model name
+      modelName: "User",
+  
+      // define the table name
+      tableName: 'users',
+  
+      // if true, add timestamps to table (updatedAt, createdAt)
+      timestamps: true,
+  
+      // if true, doesn't delete rows, instead adds a deletedAt column, if timestamps is true
+      paranoid: false,
+  
+      // if true, uses snake_case instead of camelCase to generated attributes
+      // ex: created_at instead of createdAt
+      underscored: true,
+  
+      // hooks
+      hooks: {
+        // beforeCreate: (user, options) => user.id = uuid()
+  
+      }
+    })
+  }
+
+  static associate(models) {
+    
+  }
+}
+
+export default User
