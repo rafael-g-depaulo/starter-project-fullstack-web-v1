6205f1d0a7142ed1e9ab3ee5e01ed2275b3b290d
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-07-25 16:51:24 -0300

    ✨ Add jwt crete function

--- server/Utils/jwt.js
+++ server/Utils/jwt.js
@@ -1,9 +1,17 @@
 import jwt from "jsonwebtoken"
 
+const secret = process.env.TOKEN_SECRET_KEY ?? "safsdfgksdglkjsnfl"
+
 // Transform jwt.verify into Promise format
-export const veryfy = (token, options) => new Promise((resolve, reject) => {
+export const veryfy = (token, options = {}) => new Promise((resolve, reject) => {
   jwt.verify(
-    token, process.env.TOKEN_SECRET_KEY ?? secret, options,
+    token, secret, options,
     (err, decoded) => err ? reject(err) : resolve(decoded)
   )
 })
+
+const defaultCreateOptions = {
+  expiresIn: parseInt(process.env.TOKEN_EXPIRATION_TIME ?? "1d"),
+}
+export const create = (payload, options = {}) => jwt
+  .sign(payload, secret, { ...defaultCreateOptions, ...options })
