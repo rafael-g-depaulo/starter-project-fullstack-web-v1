842453df0776ed06cd86352d6eeb1b767be1bf89
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-07-27 14:21:02 -0300

    🔧 Rename database env variables

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -5,12 +5,12 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'starter-project-strapi'),
-        username: env('DATABASE_USERNAME', 'user'),
-        password: env('DATABASE_PASSWORD', 'password'),
-        ssl: env.bool('DATABASE_SSL', false),
+        host: env('DB_HOST', '127.0.0.1'),
+        port: env.int('DB_PORT', 5432),
+        database: env('DB_NAME', 'starter-project-strapi'),
+        username: env('DB_USER', 'user'),
+        password: env('DB_PASS', 'password'),
+        ssl: env.bool('DB_SSL', false),
       },
       options: {}
     },
