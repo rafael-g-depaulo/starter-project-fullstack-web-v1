b9d1de4d1a91077c49d79e34bf515a1c78ce03fb
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-08-05 00:11:06 -0300

    ✨ Add empty app structure

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,44 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react'
+import GlobalStyles from "GlobalStyles"
 
-import logger from "@starter-project/logger"
-
-function App() {
-
-  const [ emails, setEmails ] = useState([])
-
-  useEffect(() => {
-    fetch('/api/email')
-      .then(res => res.json())
-      .then(setEmails)
-      .catch(err => console.log("err", err))
-  }, [])
-
-  logger("POGGERS!")
-
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <ul>
-        { emails.map(email => 
-          <li key={email.id}>nome: {email.nome}. email: {email.email}. fone: {email.telefone}</li>
-        )}
-        </ul>
-      </header>
-    </div>
-  );
+    <>
+      <GlobalStyles />
+      
+    </>
+  )
 }
 
-export default App;
+export default App
