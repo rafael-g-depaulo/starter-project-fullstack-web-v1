ce4b1fb356d4a88f7d5760d3e338e994947ae094
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-08-16 01:25:31 -0300

    temp

--- apps/strapi/config/database.js
+++ apps/strapi/config/database.js
@@ -11,7 +11,13 @@ module.exports = ({ env }) => {
   const port      = result ? result.groups.port     : env.int('DB_PORT', 5432)
   const database  = result ? result.groups.database : env('DB_NAME', 'starter-project-strapi')
 
-  console.log("DB CONNECTION ATTEMPT with", databaseUrl)
+  console.log("DB CONNECTION ATTEMPT with", 
+    `client: ${client}`,
+    `host: ${host}`,
+    `username: ${username}`,
+    `port: ${port}`,
+    `database: ${database}`,
+  )
   
   return {
     defaultConnection: 'default',
