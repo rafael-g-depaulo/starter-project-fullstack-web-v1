eb4d71240ae0f13961d27274e9e32a43ebac8606
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-08-28 09:12:16 -0300

    ✨ Improve DB config setup

--- apps/api/Database/config.js
+++ apps/api/Database/config.js
@@ -1,17 +1,36 @@
 import dotenv from "dotenv"
 dotenv.config()
 
-const config = {
-  dialect: "postgres",
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  define: {
-    timestamps: true,
-    underscored: true,
-  },
+export const getConfig = (uri = process.env.DATABASE_URL ?? "") => {
+  
+  const dbUriRegex = /^(?<client>postgres):\/\/(?<username>\w+):(?<password>\w+)@(?<host>.+):(?<port>\d+)\/(?<database>\w+)$/
+  const { groups = {} } = dbUriRegex.exec(uri) ?? {}
+
+  const {
+    client   = process.env.DB_CLIENT ?? "postgres",
+    host     = process.env.DB_HOST   ?? "localhost",
+    username = process.env.DB_USER   ?? "user",
+    password = process.env.DB_PASS   ?? "PASSWORD_HERE",
+    port     = process.env.DB_PORT   ?? 5432,
+    database = process.env.DB_NAME   ?? "test",
+  } = groups
+
+  return {
+    dialect: client,
+    host,
+    username,
+    password,
+    port,
+    database,
+    
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
+  }
 }
+
+const config = getConfig()
 
 export const development = config
 export const local = config
