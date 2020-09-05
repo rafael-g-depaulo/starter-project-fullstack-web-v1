d3a769f93dd250811209f7cc05180de868fdf0a6
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-09-05 01:17:40 -0300

    ✨ Add basic middlewares

--- apps/api-typescript/src/Middlewares/index.ts
+++ apps/api-typescript/src/Middlewares/index.ts
@@ -1,15 +1,18 @@
-// import cors from "./cors"
+import { NextFunction, Request, Response, Express } from 'express'
+
 import bodyParser from "body-parser"
-import { Express } from 'express'
-// import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser"
+import cors from "./cors"
+
+export type Middleware = (req: Request, res: Response, next: NextFunction) => void
 
 export default (app: Express) => {
 
-  // // to allow cors from desirable origins
-  // app.use(cors)
+  // to allow cors from desirable origins
+  app.use(cors)
 
-  // // To parse cookies from the HTTP Request
-  // app.use(cookieParser())
+  // To parse cookies from the HTTP Request
+  app.use(cookieParser())
 
   // body parser to parse http requests' bodies
   app.use(bodyParser.json({}))
