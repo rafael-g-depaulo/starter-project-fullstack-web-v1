b237a9918a5c74c3482b7f86461db1b343d04cc3
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-09-05 18:22:06 -0300

    ✨ 🔧 Add env vars to DB connection

--- apps/api-typescript/src/Db/config.ts
+++ apps/api-typescript/src/Db/config.ts
@@ -7,16 +7,13 @@ export const getDbConn = () => {
 }
 
 export const connConfig: ConnectionOptions = {
-  url: "",
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "username",
-  password: "password",
-  database: "database",
-
-  entities: [User],
-  synchronize: true,
+  url:      process.env.DATABASE_URL ?? "",
+  type:     "postgres",
+  host:     process.env.DB_HOST ?? "localhost",
+  port:     Number(process.env.DB_PORT ?? 5432),
+  username: process.env.DB_USER ?? "user",
+  password: process.env.DB_PASS ?? "password",
+  database: process.env.DB_NAME ?? "database_name",
 }
 
 export const entities = [User]
