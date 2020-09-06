8760afb71cd5dbe078d082f60373aace8da3f0e0
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-09-05 23:48:24 -0300

    🐛 Fix dbRoot used in production

--- apps/api-typescript/src/Db/config.ts
+++ apps/api-typescript/src/Db/config.ts
@@ -1,48 +1,56 @@
 import { ConnectionOptions } from "typeorm"
 
-const dbFoldersRoot = `src/Db`
+const getDbFoldersRoot = () => process.env.NODE_ENV === "development"
+  ? `src/Db`
+  : `dist/Db`
 
-export const getDbConnConfig = () => {
-
+export const getDbConnConfig: () => ConnectionOptions = () => {
+  return {
+    url:      process.env.DATABASE_URL ?? "",
+    type:     "postgres",
+    host:     process.env.DB_HOST,
+    port:     Number(process.env.DB_PORT) || undefined,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  }
 }
 
-export const connConfig: ConnectionOptions = {
-  url:      process.env.DATABASE_URL ?? "",
-  type:     "postgres",
-  host:     process.env.DB_HOST ?? "localhost",
-  port:     Number(process.env.DB_PORT ?? 5432),
-  username: process.env.DB_USER ?? "user",
-  password: process.env.DB_PASS ?? "password",
-  database: process.env.DB_NAME ?? "database_name",
-}
+export const dbConnConfig: ConnectionOptions = getDbConnConfig()
 
-export const typeOrmConfig = {
-  synchronize: true,
-  logging: false,
+export const getTypeOrmConfig = () => {
+  const dbFoldersRoot = getDbFoldersRoot()
 
-  migrations: [
-    `${dbFoldersRoot}/Migrations/**/*.ts`,
-  ],
-  subscribers: [
-    `${dbFoldersRoot}/Subscribers/**/*.ts`,
-  ],
-  cli: {
+  return {
+    synchronize: true,
+    logging: false,
+
+    migrations: [
+      `${dbFoldersRoot}/Migrations/**/*.ts`,
+    ],
+    subscribers: [
+      `${dbFoldersRoot}/Subscribers/**/*.ts`,
+    ],
+    cli: {
+      entitiesDir: `${dbFoldersRoot}/Entities`,
+      migrationsDir: `${dbFoldersRoot}/Migrations`,
+      subscribersDir: `${dbFoldersRoot}/Subscribers`,
+    },
+
+    entities: [
+      `${dbFoldersRoot}/Entities/**/*.{ts,js}`,
+    ],
+
     entitiesDir: `${dbFoldersRoot}/Entities`,
     migrationsDir: `${dbFoldersRoot}/Migrations`,
     subscribersDir: `${dbFoldersRoot}/Subscribers`,
-  },
-  
-  entities: [
-    `${dbFoldersRoot}/Entities/**/*.{ts,js}`,
-  ],
-  
-  entitiesDir: `${dbFoldersRoot}/Entities`,
-  migrationsDir: `${dbFoldersRoot}/Migrations`,
-  subscribersDir: `${dbFoldersRoot}/Subscribers`,
+  }
 }
 
+export const typeOrmConfig = getTypeOrmConfig()
+
 export const config: ConnectionOptions = {
-  ...connConfig,
+  ...dbConnConfig,
   ...typeOrmConfig,
 }
 
