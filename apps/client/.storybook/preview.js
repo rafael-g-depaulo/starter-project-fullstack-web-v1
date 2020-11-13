a78d3d5fef213ff2a21bfbfd105d0f0851ed2433
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-11-13 00:31:30 -0300

    🔧 Add globalStyles to stories

--- apps/client/.storybook/preview.js
+++ apps/client/.storybook/preview.js
@@ -1,4 +1,21 @@
+import React from "react"
+
+import { ThemeProvider } from "../src/Themes"
+import GlobalStyles from "../src/GlobalStyles"
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+const injectGlobalStylesAndTheme = Story => (
+  <>
+    <ThemeProvider>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  </>
+)
+
+export const decorators = [
+  injectGlobalStylesAndTheme,
+]
