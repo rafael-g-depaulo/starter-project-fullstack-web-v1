103ea2285d7724a999d45675700be8a4aaf3a1d8
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-11-21 23:53:46 -0300

    ✨ Add default permissions to strapi bootstrap

--- apps/strapi/config/functions/bootstrap.js
+++ apps/strapi/config/functions/bootstrap.js
@@ -10,10 +10,28 @@
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
 
+const isFirstRun = async () => {
+  const pluginStore = strapi.store({
+    environment: strapi.config.environment,
+    type: "type",
+    name: "setup"
+  });
+  const initHasRun = await pluginStore.get({ key: "initHasRun" });
+  await pluginStore.set({ key: "initHasRun", value: true });
+  return !initHasRun;
+}
+
 module.exports = async () => {
 
-  // seed database (only if in development)
-  if (process.env.NODE_ENV === "development" || process.env.STRAPI_SEED_DB === "true") {
-    require("./seeds")(strapi)
+  // if is first fun
+  if (await isFirstRun() || true) {
+
+    // setup public role
+    require("./setDefaultPermissions")(strapi)
+
+    // seed database (only if in development)
+    if (process.env.NODE_ENV === "development" || process.env.STRAPI_SEED_DB === "true") {
+      require("./seeds")(strapi)
+    }
   }
 }
