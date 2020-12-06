9070151af05c5fdbbc47274c9cca5b63d218076d
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-12-06 02:11:41 -0300

    ➕ Add user-event

--- apps/client/src/Routes/Home/HomePage/Display.test.tsx
+++ apps/client/src/Routes/Home/HomePage/Display.test.tsx
@@ -3,18 +3,22 @@ import render from "Utils/render"
 
 import HomePageDisplay from "./Display"
 import { HelloExample } from 'Api/HelloApiExample'
-import { fireEvent } from '@testing-library/react'
 
 describe("Home Page", () => {
   it("renders without exploding", () => {
     const mockedData: HelloExample = {
+      msg: ""
+    }
+    expect(() => render(<HomePageDisplay data={mockedData} />)).not.toThrow()
+  })
+
+  it("displays the data message", () => {
+    const mockedData: HelloExample = {
       msg: "mocked data test"
     }
-    const { getByText, getByTestId } = render(<HomePageDisplay data={mockedData} />)
+    const { getByText } = render(<HomePageDisplay data={mockedData} />)
 
+    // expect retrieved data to be displayed
     getByText(/"msg": "mocked data test"/)
-
-    fireEvent.click(getByTestId("data-json"))
-
   })
 })
