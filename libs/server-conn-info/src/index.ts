aacb1d3aed50c624d75ce7739c788d2c4e0d2391
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-06-02 12:42:36 -0300

    ✅ Update Cypress configuration and tests

--- libs/server-conn-info/src/index.ts
+++ libs/server-conn-info/src/index.ts
@@ -1,3 +1,5 @@
+const localhost = `0.0.0.0`
+
 // API PORT
 export const getApiPort = () => {
   const portEnv = Number(process.env.PORT || "5000")
@@ -14,7 +16,7 @@ const getApiUrlEnvVar = () => process.env.API_URL ?? process.env.REACT_APP_API_U
 export const defaultApiUrl = `starter-project-api-prod.herokuapp.com/`
 export const getApiUrl = () => process.env.NODE_ENV === 'production'
   ? getApiUrlEnvVar() ?? defaultApiUrl
-  : `http://localhost:${getApiPort()}`
+  : `http://${localhost}:${getApiPort()}`
 
 // STRAPI
 const getStrapiUrlEnvVar = () => process.env.STRAPI_URL ?? process.env.REACT_APP_STRAPI_URL
@@ -22,7 +24,7 @@ export const getStrapiPort = () => 1337
 
 export const defaultStrapiHost = `starter-project-strapi-prod.herokuapp.com/`
 export const getStrapiHost = () => process.env.NODE_ENV === 'production'
-  ? defaultStrapiHost : `localhost:${getStrapiPort()}`
+  ? defaultStrapiHost : `${localhost}:${getStrapiPort()}`
 
 export const getStrapiUrl = () => process.env.NODE_ENV === 'production'
   ? getStrapiUrlEnvVar() ?? getStrapiHost()
