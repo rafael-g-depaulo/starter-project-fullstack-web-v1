b66a0569f92d5c145834b7e5cd8ee4fcc6f870ae
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-11-20 01:29:39 -0300

    ✨ Add GlobalStyles

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react"
 
+import GlobalStyles from "./GlobalStyles"
+
 const App = () => {
   const [counter, setCounter] = useState(2)
   return (
-    <div className="App">
-      <p>test {counter}</p>
-      <button onClick={() => setCounter(c => c+1)}>increment</button>
-    </div>
+    <>
+      <div>
+        <p>test {counter}</p>
+        <button onClick={() => setCounter(c => c+1)}>increment</button>
+      </div>
+      <GlobalStyles />
+    </>
   )
 }
 
