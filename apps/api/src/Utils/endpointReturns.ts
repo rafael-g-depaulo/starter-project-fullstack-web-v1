02e0ce7da4283e7c953130996fdde8e71ca9d9aa
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-11-23 14:40:15 -0300

    ✨ Add API utitlity functions

--- apps/api/src/Utils/endpointReturns.ts
+++ apps/api/src/Utils/endpointReturns.ts
@@ -1,6 +1,15 @@
-import { errorReturn, successfulReturn } from "./responseObj"
+import { errorReturn, successfulReturn } from "./response"
 
-export const syntaticErrorReturn = errorReturn(400)
-export const unidentifiedErrorReturn = errorReturn(500)
-export const notFoundErrorReturn = errorReturn(404)
-export const updatedSuccessfullyReturn = successfulReturn(200)
+// sucessful returns
+export const updatedSuccessfully = successfulReturn(200)
+export const fetchedSuccessfully = successfulReturn(200)
+export const actionSuccessful = successfulReturn(200)
+export const createdSuccessfully = successfulReturn(201)
+
+// error returns
+export const badRequestError = errorReturn(400)
+export const unauthenticatedError = errorReturn(401)
+export const unauthorizedError = errorReturn(401)
+export const notFoundError = errorReturn(404)
+export const databaseError = errorReturn(500)
+export const unidentifiedError = errorReturn(500)
