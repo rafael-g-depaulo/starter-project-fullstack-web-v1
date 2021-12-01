e8c747483f9b00be6cadf07a2c858cbe75d78d81
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-12-01 15:17:45 -0300

    ✨ Add authentication middleware

--- apps/api/src/Routes/User/index.ts
+++ apps/api/src/Routes/User/index.ts
@@ -4,11 +4,14 @@ import { Connection } from "typeorm"
 import { Router } from "Routes"
 import UserRepository from "Repository/UserRepository"
 
+import ensureAuthenticated from "Middlewares/ensureAuthenticated"
+import ParseBody from "Middlewares/parseBody"
+
+import { loginUserSchema, registerUserSchema } from "@starter-project/entities"
+
 import RegisterUser from "./Register"
 import LoginUsers from "./Login"
 import ListUsers from "./List"
-import ParseBody from "Middlewares/parseBody"
-import { loginUserSchema, registerUserSchema } from "@starter-project/entities"
 
 type UserDeps = {
   conn: Connection
@@ -25,7 +28,7 @@ const UserRouter: Router<UserDeps> = (deps, options) => {
     .post("/register", ParseBody(registerUserSchema, "user_info"), RegisterUser({ UserRepo }))
     .post("/login", ParseBody(loginUserSchema, "user_info"), LoginUsers({ UserRepo }))
     // .post("/logout", RegisterUser({ UserRepo }))
-    .get("/", ListUsers({ UserRepo }))
+    .get("/", ensureAuthenticated, ListUsers({ UserRepo }))
 }
 
 export default UserRouter
