adc33a96d2f4cf28a63ee945f8ea961a053f2ff6
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-12-28 01:12:58 -0300

    ✨ Add login form

---
+++ apps/client/src/Organisms/LoginForm/index.tsx
@@ -0,0 +1,59 @@
+import { FC, useCallback } from "react"
+import { Formik, Form } from "formik"
+
+import { loginUserSchema, UserLogin } from "@starter-project/entities"
+import { useLogin } from "Api/LoginUser"
+
+import EmailInputGroup from "Molecules/InputGroup/Email"
+import PasswordInputGroup from "Molecules/InputGroup/Password"
+import { FormikOnSubmit } from "Utils/formik"
+import { useCurrentUser } from "Context/LoggedUserToken"
+
+export interface LoginFormProps {
+  
+}
+
+const initialValues: UserLogin = {
+  email: "",
+  password: "",
+}
+
+export const LoginForm: FC<LoginFormProps> = () => {
+  const { mutateAsync, error, data } = useLogin()
+  const { setToken, token } = useCurrentUser()
+
+  const onSubmit = useCallback<FormikOnSubmit<UserLogin>>((values, { }) =>
+    mutateAsync(values)
+      .then(user => setToken(user.token))
+  , [])
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={loginUserSchema}
+    >
+      {({ values, errors }) => (
+        <Form>
+          <EmailInputGroup
+            label="Email:"         
+          />
+
+          <PasswordInputGroup
+            label="Senha:"
+            name="password"
+          />
+
+          <pre>values: {JSON.stringify(values, null, 2)}</pre>
+          <pre>errors: {JSON.stringify({ ...errors, api: error}, null, 2)}</pre>
+          <pre>loggedIn? {`${!!token}`}</pre>
+          <pre>JWT: {token}</pre>
+          <pre>return data: {JSON.stringify(data, null, 2)}</pre>
+          <button type="submit">submit</button>
+        </Form>
+      )}
+    </Formik>
+  )
+}
+
+export default LoginForm
