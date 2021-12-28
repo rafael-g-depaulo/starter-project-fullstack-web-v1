9b08834b64794d5dbc65b3320e01caba3d26629f
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-12-27 23:35:25 -0300

    ♻️ Refactor code that storybook compiled badly

--- apps/client/src/FeatureFlags.ts
+++ apps/client/src/FeatureFlags.ts
@@ -1,6 +1,8 @@
-const UseEnvVar = (varName: string, defaultValue: boolean) => 
-  process.env[varName] === "true" ? true :
-  process.env[varName] === "false" ? false : defaultValue
+const UseEnvVar = (varName: string, defaultValue: boolean) => {
+  const envVar = process.env[varName]
+  return envVar === "true" ? true :
+    envVar === "false" ? false : defaultValue
+}
 
 export const showRoutes = UseEnvVar("REACT_APP_SHOW_ROUTES", true)
 
