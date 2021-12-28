21ee326f71a7f2cec4bec6a61e347d5062af728d
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-12-28 01:28:59 -0300

    ✅ Fix tests

--- libs/entities/src/User.test.ts
+++ libs/entities/src/User.test.ts
@@ -1,9 +1,10 @@
-import { userSchema } from "."
+import { Roles } from "@starter-project/permissions"
+import { userSchema, SerializedUser } from "."
 
 describe("user entity", () => {
 
-  it('should have email and id', async () => {
-    const testData = { id: 'n1g9O3Rxw5', email: 'test@mail.com' }
+  it('should have email, role and id', async () => {
+    const testData: SerializedUser = { id: 'n1g9O3Rxw5', email: 'test@mail.com', role: Roles.LOGGED_USER }
     expect(await userSchema.isValid(testData)).toBe(true)
   })
 
